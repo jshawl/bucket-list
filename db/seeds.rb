0a87cec7ae749fde45c04938172fd8f282ed90fe
Jesse Shawl <jesse@jshawl.com>
2015-04-13 10:49:26 -0400

    add seeds and model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Item.destroy_all
+Item.create([
+  { title: "Visit France", description: "Make sure the king of France is bald." },
+  { title: "Own fewer than 100 things", description: "Minimalize." },
+  { title: "Play a show on stage", description: "Be the rockstar you are." }
+])
